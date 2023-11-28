26da666a0d13d92adb96d0a0b559fc263ce94729
Stepanova Vassa <vstepanova@fab.pasona.tech>
2023-11-28 16:23:45 +0900

    feat: functions to make czml data for different points and animated on time

--- make_package.sh
+++ make_package.sh
@@ -1,13 +1,13 @@
 # Get the current date and format it as YYYYMMDD
 DATE=$(date +"%Y%m%d")
 # Rename arcgis-connector.html to arcgis-connector.js
-mv 3DGraph.html 3DGraph.js
+mv diagram-graph.html diagram-graph.js
 
-if [ -f 3DGraph-$DATE.zip ]; then
-  zip -u 3DGraph-$DATE.zip 3DGraph.js reearth.yml
+if [ -f diagram-graph-$DATE.zip ]; then
+  zip -u diagram-graph-$DATE.zip diagram-graph.js reearth.yml
 else
-  zip 3DGraph-$DATE.zip 3DGraph.js reearth.yml
+  zip diagram-graph-$DATE.zip diagram-graph.js reearth.yml
 fi
 
-# Rename 3DGraph.js to its original name
-mv 3DGraph.js 3DGraph.html
+# Rename diagram-graph.js to its original name
+mv diagram-graph.js diagram-graph.html
